9b4778132dc0b7e9c4048e144fc9f72761de15c5
melinamorales512 <melinamorales956@gmail.com>
2022-12-01 18:18:43 -0600

    heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlroutes');
 
 // Parse URL encoded & JSON
 app.use(express.urlencoded({ extended: true }));
